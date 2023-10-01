e03a16db32b1832f69ab66a08c07131ff2f137a8
Dani Sandoval <hi@dreamindani.com>
2023-09-30 18:38:33 -0600

    content

---
+++ src/lib/Band.svelte
@@ -0,0 +1,50 @@
+<script>
+	/**
+	 * @type {String}
+	 */
+	export let img;
+	/**
+	 * @type {String}
+	 */
+	export let name;
+	/**
+	 * @type {String}
+	 */
+	export let years;
+	/**
+	 * @type {String}
+	 */
+	export let link;
+</script>
+
+<a class="band" href={link}>
+	<img src={img} alt={`Image representing ${name}`} />
+	<h2>{name}<br />({years})</h2>
+</a>
+
+<style>
+	a {
+		display: block;
+		text-decoration: none;
+		color: #24292f;
+	}
+
+	a:hover {
+		color: #77bf51;
+	}
+
+	h2 {
+		text-align: center;
+		font-family: Raleway;
+		font-size: 32px;
+		font-style: normal;
+		font-weight: 500;
+		line-height: normal;
+	}
+
+	img {
+		width: 100%;
+		border-radius: 4px;
+		box-shadow: -4px 4px 4px 0px rgba(0, 0, 0, 0.12);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Band from '$lib/Band.svelte';
+</script>
+
+<h1>My Music</h1>
+<div class="container">
+	<Band
+		img="puppy.jpg"
+		name="Puppy on Parade"
+		years="2023–Present"
+		link="https://open.spotify.com/artist/4uTDRgVhgPJx7jv3s7ZEGM"
+	/>
+	<Band
+		img="life.jpg"
+		name="Life After Parties"
+		years="2019–2021"
+		link="https://youtu.be/NCk3BX-rPoM"
+	/>
+	<Band
+		img="local.jpg"
+		name="Local Point"
+		years="2015–2016"
+		link="https://localpoint.bandcamp.com/"
+	/>
+	<Band
+		img="daniel.jpg"
+		name="Daniel Sandoval"
+		years="2014–2022"
+		link="https://open.spotify.com/artist/47mXmNmm2ekoxyCwowItaX?si=fVM4Puc9TFmu1Bcxb5C7hg"
+	/>
+</div>
+
+<style>
+	:global(body) {
+		margin: 52px;
+	}
+	h1 {
+		color: #326990;
+		font-family: Raleway;
+		font-size: 60px;
+		font-style: normal;
+		font-weight: 700;
+		line-height: normal;
+		letter-spacing: -0.3px;
+	}
+	.container {
+		display: flex;
+		width: 100%;
+		gap: 24px;
+	}
+	@media (max-width: 800px) {
+		:global(body) {
+			margin: 24px;
+		}
+		.container {
+			flex-direction: column;
+		}
+		h1 {
+			font-size: 40px;
+		}
+	}
+</style>
